fd488918afb6944513cdcd17313b401cadbe0a1b
William Kennedy <bill@ardanlabs.com>
2021-02-23 14:05:56 -0500

    day2 commit5

---
+++ business/mid/logger.go
@@ -0,0 +1,26 @@
+package mid
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/ardanlabs/service/foundation/web"
+)
+
+// Logger ...
+func Logger(log *log.Logger) web.Middleware {
+	m := func(handler web.Handler) web.Handler {
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			err := handler(ctx, w, r)
+
+			log.Println(r)
+
+			return err
+		}
+
+		return h
+	}
+
+	return m
+}
