fd1402576d2b711d982b4edc345f220d3632fb30
William Kennedy <bill@ardanlabs.com>
2021-02-25 15:17:53 -0500

    day4 commit5

--- app/sales-api/handlers/check.go
+++ app/sales-api/handlers/check.go
@@ -2,23 +2,34 @@ package handlers
 
 import (
 	"context"
-	"errors"
-	"math/rand"
 	"net/http"
 
-	"github.com/ardanlabs/service/business/validate"
+	"github.com/ardanlabs/service/foundation/database"
 	"github.com/ardanlabs/service/foundation/web"
+	"github.com/jmoiron/sqlx"
 )
 
-func readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	if n := rand.Intn(100); n%2 == 0 {
-		return validate.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
+type checkGroup struct {
+	build string
+	db    *sqlx.DB
+}
+
+// readiness checks if the database is ready and if not will return a 500 status.
+// Do not respond by just returning an error because further up in the call
+// stack it will interpret that as a non-trusted error.
+func (cg checkGroup) readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	status := "ok"
+	statusCode := http.StatusOK
+	if err := database.StatusCheck(ctx, cg.db); err != nil {
+		status = "db not ready"
+		statusCode = http.StatusInternalServerError
 	}
 
-	status := struct {
-		Status string
+	health := struct {
+		Status string `json:"status"`
 	}{
-		Status: "OK",
+		Status: status,
 	}
-	return web.Respond(ctx, w, status, http.StatusOK)
+
+	return web.Respond(ctx, w, health, statusCode)
 }
